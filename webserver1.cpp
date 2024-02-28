a9daba59974a1c8947485232c399360cdb22c22d
JacobGH2 <jhelhoski253@gmail.com>
2024-02-27 22:03:29 -0500

    part 1 now runs in loop

--- webserver1.cpp
+++ webserver1.cpp
@@ -35,71 +35,73 @@ int main(int argc, char **argv) {
     serverAddress.sin_addr.s_addr = INADDR_ANY;
     bind(serverSocket, (const struct sockaddr*)&serverAddress, sizeof(serverAddress));
 
-    // listen, accept, and receive
-    if (listen(serverSocket, 1) < 0) {
-        cerr << "Listen failed" << endl;
-        return 1;
-    }
-    int clientSocket = accept(serverSocket, nullptr, nullptr);
-    if (clientSocket < 0) {
-        cerr << "Accept failed" << endl;
-        return 1;
-    }
-    char buffer[1024] = {0};
-    recv(clientSocket, buffer, sizeof(buffer), 0);
-    
-    // output requested object to terminal and trim off leading "/"
-    char * tok;
-    tok = strtok(buffer, " ");
-    tok = strtok(NULL, " ");
-    string object(tok);
-    object.erase(object.begin());
-
-    // get file type of object
-    char *type;
-    if (object != "") {
-        type = strtok(tok, ".");
-        type = strtok(NULL, ".");
-    }
-    string typeStr(type);
-    
-    // set the response string based on the request
-    string response;
-    if (object != "") {
-        if (access(object.c_str(), F_OK) == -1) {
-            response = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 22\r\n\r\n404: Content Not Found";
-            goto send;
+    while (true) {
+        // listen, accept, and receive
+        if (listen(serverSocket, 1) < 0) {
+            cerr << "Listen failed" << endl;
+            return 1;
         }
-    }
+        int clientSocket = accept(serverSocket, nullptr, nullptr);
+        if (clientSocket < 0) {
+            cerr << "Accept failed" << endl;
+            return 1;
+        }
+        char buffer[1024] = {0};
+        recv(clientSocket, buffer, sizeof(buffer), 0);
 
-    if (object == "") {
-        // replies with plain text
-        response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 5\r\n\r\nHello";
-    } else if (typeStr == "html") {
-        // html file handler
-        ifstream infile(object);
-        stringstream ss;
-        ss << infile.rdbuf();
-        string html_str = ss.str();
-        response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: " + to_string(html_str.length())+ "\r\n\r\n"+ html_str;
-    } else if (typeStr == "pdf") {
-        // pdf file handler
-        ifstream infile (object);
-        stringstream ss;
-        ss << infile.rdbuf();
-        string pdf_str = ss.str();
-        response = "HTTP/1.1 200 OK\r\nContent-Type: application/pdf\r\nContent-Length: " + to_string(pdf_str.length())+ "\r\n\r\n"+ pdf_str;
-    }
+        // output requested object to terminal and trim off leading "/"
+        char * tok;
+        tok = strtok(buffer, " ");
+        tok = strtok(NULL, " ");
+        string object(tok);
+        object.erase(object.begin());
 
-    send:
-    // send the response
-    if (send(clientSocket, response.c_str(), response.length(), 0) < 0) {
-        cerr << "Send failed" << endl;
-        return 1;
-    }
+        // get file type of object
+        char *type;
+        if (object != "") {
+            type = strtok(tok, ".");
+            type = strtok(NULL, ".");
+        }
+        string typeStr(type);
 
-    // close sockets
-    close(clientSocket);
+        // set the response string based on the request
+        string response;
+        if (object != "") {
+            if (access(object.c_str(), F_OK) == -1) {
+                response = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 22\r\n\r\n404: Content Not Found";
+                goto send;
+            }
+        }
+
+        if (object == "") {
+            // replies with plain text
+            response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 5\r\n\r\nHello";
+        } else if (typeStr == "html") {
+            // html file handler
+            ifstream infile(object);
+            stringstream ss;
+            ss << infile.rdbuf();
+            string html_str = ss.str();
+            response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: " + to_string(html_str.length())+ "\r\n\r\n"+ html_str;
+        } else if (typeStr == "pdf") {
+            // pdf file handler
+            ifstream infile (object);
+            stringstream ss;
+            ss << infile.rdbuf();
+            string pdf_str = ss.str();
+            response = "HTTP/1.1 200 OK\r\nContent-Type: application/pdf\r\nContent-Length: " + to_string(pdf_str.length())+ "\r\n\r\n"+ pdf_str;
+        }
+
+        send:
+        // send the response
+        if (send(clientSocket, response.c_str(), response.length(), 0) < 0) {
+            cerr << "Send failed" << endl;
+            return 1;
+        }
+
+        // close sockets
+        close(clientSocket);
+    }
     close(serverSocket);
 
     return 0;
